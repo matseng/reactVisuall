65d988fbf489b060e1d3764dfa772030ba54590b
Michael Tseng <matseng@gmail.com>
2017-06-07 11:41:50 -0700

    able to pass credentials from objC google login to react native js and log into firebase

--- index.ios.js
+++ index.ios.js
@@ -8,15 +8,44 @@ import {
   View
 } from 'react-native';
 
+import firebaseConfig from './FirebaseConfig'
+
+import * as firebase from 'firebase';
+// Initialize Firebase
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCP1-Ut_BXCDgfhH7d861Bq3N-oUubR1tU",
+//     authDomain: "visuall-2f878.firebaseapp.com",
+//     databaseURL: "https://visuall-2f878.firebaseio.com",
+//     projectId: "visuall-2f878",
+//     storageBucket: "visuall-2f878.appspot.com",
+//     messagingSenderId: "547811396909"
+//   };
+firebase.initializeApp(firebaseConfig);
+
+function myCredential(idToken, accessToken)
+{
+  const credential = firebase
+      .auth
+      .GoogleAuthProvider
+      .credential(idToken, accessToken);
+
+  firebase
+      .auth()
+      .signInWithCredential(credential)
+      .then(() => console.log('Account accepted'))
+      .catch((error) => console.log('Account disabled'));
+}
+
 class RNHighScores extends React.Component {
   render() {
+    myCredential(this.props['idToken'], this.props['accessToken']);
     var contents = this.props["scores"].map(
       score => <Text key={score.name}>{score.name}:{score.value}{"\n"}</Text>
     );
     return (
       <View style={styles.container}>
         <Text style={styles.highScoresTitle}>
-          2048 High Scores + Testing Script v02!
+          2048 High Scores + Testing Script v04!
         </Text>
         <Text style={styles.scores}>
           {contents}
